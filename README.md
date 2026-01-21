ad8bf15c1c83440db4e29be415a09730495ba5c2
pietow <59700795+pietow@users.noreply.github.com>
2026-01-21 10:14:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + TypeScript + Vite
+
+````shell
+gh repo create react_css --template pietow/React_template_alpha --private
+gh repo view  https://github.com/pietow/react_css --json sshUrl -q .sshUrl
+````
+
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+import prettier from './eslint-prettier.config.js'
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'react/prop-types': 'off',
+    },
+  },
+])
